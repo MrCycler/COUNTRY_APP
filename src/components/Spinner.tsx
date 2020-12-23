a5c558d896cd9f95f98746d1fa505fc3be2a12d2
Juan Mendoza <mendoza.jm@pucp.edu.pe>
2020-12-23 16:16:17 -0500

    add: first version of spinner

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,5 +1,22 @@
+// react import
 import React from "react";
+// styled components module import
+import styled from 'styled-components';
+
 import gif from "../assets/images/spinner.gif";
 
-const Spinner = () => <img src={gif} alt="spinner" />
+const SpinnerDiv = styled.div`
+    height:100vh;
+    width:100%;
+    display:flex;
+    align-items:center;
+    justify-content: center;
+`
+const SpinnerImg = styled.img`
+    margin:auto;
+`
+
+const Spinner = () => 
+<SpinnerDiv>
+<SpinnerImg src={gif} alt="spinner" /></SpinnerDiv>
 export default Spinner
