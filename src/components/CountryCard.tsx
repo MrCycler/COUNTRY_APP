f8552b87294832670483e342ca7eade542394d3f
Juan Mendoza <mendoza.jm@pucp.edu.pe>
2020-12-24 23:09:54 -0500

    add: loading flags with scroll reveal

--- src/components/CountryCard.tsx
+++ src/components/CountryCard.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import styled from "styled-components";
+import React from "react"
+import styled from "styled-components"
 //import AOS from 'aos';
 //import 'aos/dist/aos.css';
 
@@ -16,17 +16,18 @@ const CardImage = styled.img`
   margin-bottom: 2rem;
   margin-right: auto;
   margin-left: auto;
-  z-index:1;
+  z-index: 1;
 `
 
 export default function CountryCard(props) {
   return (
-    
-      <CardImage
-        src={props.image}
-        alt="MIT"
-        
-      />
-
+    <CardImage
+      src={props.image}
+      alt="MIT"
+      data-sal="slide-up"
+      data-sal-delay="0"
+      data-sal-duration="800"
+      data-sal-easing="ease-in-out"
+    />
   )
 }
