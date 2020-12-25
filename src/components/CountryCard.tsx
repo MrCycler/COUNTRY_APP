ead0fc7c8008910c8184e008d16461555900d142
Juan Mendoza <mendoza.jm@pucp.edu.pe>
2020-12-24 23:47:24 -0500

    edit: initial animation

--- src/components/CountryCard.tsx
+++ src/components/CountryCard.tsx
@@ -1,13 +1,5 @@
 import React from "react"
 import styled from "styled-components"
-//import AOS from 'aos';
-//import 'aos/dist/aos.css';
-
-//AOS.init();
-
-/*data-aos="fade-up"
-        data-aos-delay="50"
-        data-aos-duration="1500" */
 
 const CardImage = styled.img`
   width: 150px;
@@ -17,17 +9,23 @@ const CardImage = styled.img`
   margin-right: auto;
   margin-left: auto;
   z-index: 1;
+  animation: 2s ease-out 0s 1 slideInFromLeft;
+
+  @keyframes slideInFromLeft {
+  0% {
+    transform: translateY(200%);
+  }
+  100% {
+    transform: translateY(0);
+  }
+}
 `
 
 export default function CountryCard(props) {
   return (
     <CardImage
       src={props.image}
-      alt="MIT"
-      data-sal="slide-up"
-      data-sal-delay="0"
-      data-sal-duration="800"
-      data-sal-easing="ease-in-out"
+      alt="country"           
     />
   )
 }
